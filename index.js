a3f315f6e107d74e42d5834e14644c254ea46920
Gonza <lopezgonzalo834@gmail.com>
2026-03-10 10:26:00 -0300

    mejoras

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
+
+
 window.onload = function() {
-    const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+    // Array de numeros y palos
+    const numero = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
     const palo  = ['♠', '♣', '♥', '♦'];
     
-    // Selecciona valor y palo al azar
-    const randomValue = values[Math.floor(Math.random() * values.length)];
+    // Seleccion aleatoria de numero y palo
+    const randomNumero = numero[Math.floor(Math.random() * numero.length)];
     const randomPalo = palo[Math.floor(Math.random() * palo.length)];
-    
-    // Pone el color según el palo 
+
+    // Asignacion de color segun el palo
     const color = (randomPalo === '♥' || randomPalo === '♦') ? 'red' : 'black';
-    
-    // Asigna el valor de la carta al elemento HTML
-    const numberElement = document.querySelector('.number');
-    numberElement.innerHTML = randomValue;
-    numberElement.style.color = color;
-    
-    // Asigna el palo y el color a los contenedores superior e inferior
-    const topSuit = document.querySelector('.top-carta');
-    const bottomSuit = document.querySelector('.bottom-carta');
-    
-    topSuit.innerHTML = randomPalo;
-    topSuit.style.color = color;
-    
-    bottomSuit.innerHTML = randomPalo;
-    bottomSuit.style.color = color;
+
+    // Seleccionamos los elementos del DOM
+    const numeroElement = document.querySelector(".number")
+    const paloElementTop = document.querySelector(".top")
+    const paloElementBottom = document.querySelector(".bottom")
+    const cartaElement = document.querySelector(".card")
+
+    // Asignamos el valor y el color 
+    numeroElement.textContent = randomNumero;
+    paloElementTop.textContent = randomPalo;
+    paloElementBottom.textContent = randomPalo;
+    cartaElement.style.color = color;
+
 };
